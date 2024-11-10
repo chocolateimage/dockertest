bd5fadbf93d1a5963cc43d77610ebaf21f96c053
chocolateimage <chocolateimage@protonmail.com>
2024-11-10 11:51:48 +0100

    Remove unnecessary SIGINT handler

--- jsapi/src/index.js
+++ jsapi/src/index.js
@@ -1,12 +1,6 @@
-const process = require('process')
 const express = require('express')
 const app = express()
 const port = 8083
-
-process.on('SIGINT', function() {
-  console.log( "\nGracefully shutting down from SIGINT (Ctrl-C)" );
-  process.exit(0);
-});
 
 app.post('/jsapi/data', (req, res) => {
   res.send('This is some data from express')
