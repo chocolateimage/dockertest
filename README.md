26a5a9f41ec10dd9a30fbc9eceff7bfd82f44b94
chocolateimage <chocolateimage@protonmail.com>
2024-11-10 11:51:36 +0100

    Add information of --build

--- README.md
+++ README.md
@@ -5,5 +5,7 @@ Please do **NOT** use this, it's just an example for me to know pnpm + docker + 
 If you want to know how to use pnpm with docker use this instead: https://pnpm.io/docker#example-2-build-multiple-docker-images-in-a-monorepo
 
 
-- Dev: `docker compose up --watch`
+- Dev: `docker compose up --watch --build`
 - Prod: `docker compose -f compose.yml -f compose-production.yml up --build`
+
+Build is needed when changing between dev and prod because Docker forgets that it has been changed.
--- ui/src/App.vue
+++ ui/src/App.vue
@@ -26,8 +26,9 @@ onMounted(() => {
     <h4>With an API (just using Python + Flask): <span class="data">{{ data }}</span></h4>
     <h4>With an API (just using pnpm + nodemon + express): <span class="data">{{ jsdata }}</span></h4>
     <hr>
-    <h4>Dev: <code>docker compose up --watch</code> (watch is Docker's development thing)</h4>
+    <h4>Dev: <code>docker compose up --watch --build</code> (watch is Docker's development thing)</h4>
     <h4>Prod: <code>docker compose -f compose.yml -f compose-production.yml up --build</code></h4>
+    <p>Build is needed when changing between dev and prod because Docker forgets that it has been changed.</p>
   </div>
 </template>
 <style scoped>
